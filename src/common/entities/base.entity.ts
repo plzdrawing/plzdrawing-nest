fde7babb7dfc9eb06e7fd226ed7bde96bc986126
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2026-01-04 23:23:21 +0900

    feat: 시간 직렬화

--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -1,9 +1,13 @@
 import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Transform } from 'class-transformer';
+import * as moment from 'moment';
 
 export abstract class BaseEntity {
   @CreateDateColumn({ name: 'created_at' })
+  @Transform(({ value }) => moment(value).format('YYYY-MM-DD HH:mm:ss'))
   createdAt: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
+  @Transform(({ value }) => moment(value).format('YYYY-MM-DD HH:mm:ss'))
   updatedAt: Date;
 }
